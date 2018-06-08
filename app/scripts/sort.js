88f17e028f55910bdec46ea0221689601b205b9f
Joanna Karwan <asia.karwan@gmail.com>
2018-06-08 12:45:50 +0200

    GitHub Pages prepare

--- app/scripts/sort.js
+++ app/scripts/sort.js
@@ -3,20 +3,12 @@ var SORT = {
     var sortEl = document.getElementById("sort");
     var sortDropdown = document.getElementsByClassName("sort__dropdown")[0];
 
-    sortEl.addEventListener(
-      "click",
-      function() {
-        sortDropdown.classList.toggle("sort__dropdown--active");
-      },
-      false
-    );
-    sortDropdown.addEventListener(
-      "click",
-      function() {
-        sortDropdown.classList.remove("sort__dropdown--active");
-      },
-      false
-    );
+    sortEl.addEventListener("click", function() {
+      sortDropdown.classList.toggle("sort__dropdown--active");
+    });
+    sortDropdown.addEventListener("click", function() {
+      sortDropdown.classList.remove("sort__dropdown--active");
+    });
   },
 
   init: function() {
