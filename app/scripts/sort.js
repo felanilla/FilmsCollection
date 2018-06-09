5e6052dd644d12f3240f559d1f1615d082da0f33
Joanna Karwan <asia.karwan@gmail.com>
2018-06-09 20:45:07 +0200

    animations

--- app/scripts/sort.js
+++ app/scripts/sort.js
@@ -2,12 +2,30 @@ var SORT = {
   openDropdown: () => {
     var sortEl = document.getElementById("sort");
     var sortDropdown = document.getElementsByClassName("sort__dropdown")[0];
+    var arrow = document.getElementsByClassName("sort__title__arrow")[0];
 
-    sortEl.addEventListener("click", function() {
-      sortDropdown.classList.toggle("sort__dropdown--active");
-    });
-    sortDropdown.addEventListener("click", function() {
-      sortDropdown.classList.remove("sort__dropdown--active");
+    //detect click inside and outside element
+    document.addEventListener("click", function() {
+      var hasParent = false;
+      for (
+        var node = event.target;
+        node != document.body;
+        node = node.parentNode
+      ) {
+        if (node.id == "sort") {
+          hasParent = true;
+          break;
+        }
+      }
+      if (hasParent) {
+        sortDropdown.classList.toggle("sort__dropdown--active");
+        sortEl.classList.toggle("u-box-shadow");
+        arrow.classList.toggle("rotate");
+      } else {
+        sortDropdown.classList.remove("sort__dropdown--active");
+        sortEl.classList.remove("u-box-shadow");
+        arrow.classList.remove("rotate");
+      }
     });
   },
 
